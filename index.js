4dfb91d460e16aca7268e7d34fd16b998c30658a
blake <blake.fang@letote.cn>
2018-09-05 15:39:41 +0800

    time 字段冲突

--- index.js
+++ index.js
@@ -1,6 +1,62 @@
+import { NativeModules } from 'react-native'
 
-import { NativeModules } from 'react-native';
+const { RNSensors } = NativeModules
 
-const { RNSensors } = NativeModules;
+const formatData = data => {
+  if (data['time']) {
+    data._time = data['time']
+    data.time = undefined
+    delete data.time
+  }
+  return data
+}
+const formatEventString = event => {
+  if (event == 'time') return '_time'
+  else return event
+}
+export default class Sensors {
+  static trackWithProperties(event, data) {
+    RNSensors.trackWithProperties(formatEventString(event), formatData(data))
+  }
 
-export default RNSensors;
+  static enableReactNativeAutoTrack() {
+    RNSensors.enableReactNativeAutoTrack()
+  }
+  static trackViewScreen(data) {
+    RNSensors.trackViewScreen(formatData(data))
+  }
+  static track(event) {
+    RNSensors.track(formatEventString(event))
+  }
+
+  static trackTimerStart(event) {
+    RNSensors.trackTimerStart(formatEventString(event))
+  }
+  static trackTimerEnd(event, data) {
+    RNSensors.trackTimerEnd(formatEventString(event), formatData(data))
+  }
+  static setPublicEvent(event, data) {
+    RNSensors.setPublicEvent(formatEventString(event), formatData(data))
+  }
+  static login(id) {
+    RNSensors.login(id)
+  }
+  static profileSet(data) {
+    RNSensors.profileSet(formatData(data))
+  }
+  static profileSetOnce(data) {
+    RNSensors.profileSetOnce(formatData(data))
+  }
+  static profileIncrement(data) {
+    RNSensors.profileIncrement(formatData(data))
+  }
+  static profileAppend(event, data) {
+    RNSensors.profileAppend(formatEventString(event), formatData(data))
+  }
+  static identify(identify) {
+    RNSensors.identify(identify)
+  }
+  static enableAutoTrack() {
+    RNSensors.enableAutoTrack()
+  }
+}
