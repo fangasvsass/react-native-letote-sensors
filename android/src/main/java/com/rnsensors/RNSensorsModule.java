cb56534d10709548df6fcc84dc23d400b9dcaf48
fangasvsass <fffffangqi@gmail.com>
2019-11-21 13:40:07 +0800

    update android

--- android/src/main/java/com/rnsensors/RNSensorsModule.java
+++ android/src/main/java/com/rnsensors/RNSensorsModule.java
@@ -1,6 +1,6 @@
 
 package com.rnsensors;
-
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
